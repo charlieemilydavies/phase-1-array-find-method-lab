c1ec4c57af57c4596d2b18885b6b45c7429f349d
Charlie Davies <charlieemilydavies@gmail.com>
2024-08-27 11:06:13 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+  // Find the first record where the result is "W"
+  const win = record.find((entry) => entry.result === "W");
+
+  // If a win is found, return the year, otherwise return undefined
+  return win ? win.year : undefined;
+}
